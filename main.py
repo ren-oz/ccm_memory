5874d7b1a260ad02bf0d3ca4c75b54cb4210d484
Ren Ozen <renanozen@cmail.carleton.ca>
2022-12-28 16:07:11 -0500

    refactored & further abstracted MatrixMemory; added MINERVA2 and AttentionMemory classes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from hrrlib import HRR, rand
-from abstract import AbstractMemoryItem, AbstractMemory
+from abstract import AbstractMemoryItem, AbstractMemory, ABC, abstractmethod
 import numpy as np
 from utils import softmax
 from typing import Union
@@ -11,7 +11,13 @@ class Vector(np.ndarray, AbstractMemoryItem): ...
 class Activation(Vector): ...
 
 
-class MatrixMemory(AbstractMemory):
+class AbstractMatrixMemory(AbstractMemory, ABC):
+    @abstractmethod
+    def activation(self, probe: Vector) -> Activation: ...
+    
+    @abstractmethod
+    def apply_activation(self, activation: Activation) -> Vector: ...
+
     def __init__(self, matrix:np.ndarray=None):
         self._memory = matrix
     
@@ -22,10 +28,10 @@ class MatrixMemory(AbstractMemory):
         else:
             self._memory = np.concatenate((self._memory, v.reshape((1, v.size))))
     
-    def retrieve(self, item: AbstractMemoryItem) -> AbstractMemoryItem:
-        # some kind of check up the inheritance hierarchy to determine which error msg to display
-        err = f'Must implement this function in class "{self.__class__.__name__}"'
-        raise NotImplementedError(err)
+    def retrieve(self, probe: Vector) -> Vector:
+        if self._memory is None:
+            return Vector(0)
+        return self.apply_activation(self.activation(probe)).view(Vector)
         
     def delete(self, index:int) -> None:
         self._memory = self._memory[[i for i in range(self._memory.shape[0]) if i != index]]
@@ -39,7 +45,7 @@ class MatrixMemory(AbstractMemory):
         self._memory = np.ndarray(memory)
 
 
-class AttentionMemory(MatrixMemory):
+class AttentionMemory(AbstractMatrixMemory):
 
     def __init__(self, matrix:np.ndarray=None, beta:float=1):
         super().__init__(matrix=matrix)
@@ -47,7 +53,28 @@ class AttentionMemory(MatrixMemory):
 
     def activation(self, probe: Vector) -> Activation:
         dot = self._memory@probe
-        return softmax(self._beta*dot)
+        return softmax(self._beta*dot).view(Activation)
 
-    def retrieve(self, activation: Activation) -> Vector:
-        return self._memory.T@activation
+    def apply_activation(self, activation: Activation) -> Vector:
+        return (self._memory.T@activation).view(Vector)
+
+
+class MINERVA2(AbstractMatrixMemory):
+
+    def __init__(self, matrix:np.ndarray=None, exponent=3):
+        super().__init__(matrix=matrix)
+        self._exponent = exponent
+
+    def activation(self, probe: Vector) -> Activation:
+        probe = np.array(probe)
+        # find the max number of non-zero entries between probe and memory item
+        n = [
+            max(np.where(self._memory[i,:] == 0)[0].size, np.where(probe == 0)[0].size) 
+            for i in range(self._memory.shape[0])
+        ]
+        scale = (np.ones(len(self._memory))*self._memory.shape[1])-n
+        dot_scaled = (self._memory@probe)/scale
+        return (dot_scaled**self._exponent).view(Activation)
+
+    def apply_activation(self, activation: Activation) -> Vector:
+        return (self._memory.T@activation).view(Vector)
